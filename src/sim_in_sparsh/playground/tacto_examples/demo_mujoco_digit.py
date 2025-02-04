d39ac186ce61e601ca2db1a2f7e74187fec3ed88
ayada <abdallahayman772@gmail.com>
2025-02-04 10:41:29 +0100

    experimental model; experimental tacto-mujoco integration;  some cleaning; ignore data

---
+++ src/sim_in_sparsh/playground/tacto_examples/demo_mujoco_digit.py
@@ -0,0 +1,124 @@
+# Copyright (c) Facebook, Inc. and its affiliates.
+
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+import logging
+import os
+import sys
+import time
+
+import cv2
+import hydra
+from omegaconf import OmegaConf
+import pybullet as p
+import pybulletX as px
+import mujoco as mj
+import pyrender
+import tacto  # Import TACTO
+from mujoco.viewer import launch
+import glfw
+import threading
+
+CONTROL_INCREMENT = 0.0005  # Amount to move joints per keypress
+
+def key_callback(window, key, scancode, action, mods):
+    """
+    GLFW key callback to interactively control joint positions.
+    """
+    if action != glfw.PRESS and action != glfw.REPEAT:
+        return  # Ignore key releases
+
+    if key == glfw.KEY_PAGE_UP:
+        data.qpos[model.joint("x").qposadr] += CONTROL_INCREMENT
+    elif key == glfw.KEY_PAGE_DOWN:
+        data.qpos[model.joint("x").qposadr] -= CONTROL_INCREMENT
+    elif key == glfw.KEY_RIGHT:
+        data.qpos[model.joint("z").qposadr] += CONTROL_INCREMENT
+    elif key == glfw.KEY_LEFT:
+        data.qpos[model.joint("z").qposadr] -= CONTROL_INCREMENT
+    elif key == glfw.KEY_UP:
+        data.qpos[model.joint("y").qposadr] += CONTROL_INCREMENT
+    elif key == glfw.KEY_DOWN:
+        data.qpos[model.joint("y").qposadr] -= CONTROL_INCREMENT
+
+log = logging.getLogger(__name__)
+
+# Load the config YAML file from examples/conf/digit.yaml
+# OmegaConf.register_new_resolver("base_dir", script_dir)
+# @hydra.main(config_path="conf", config_name="digit")
+# script meta data, configuration and constants
+# remove leading slash from string
+def main(cfg):
+    # change base_dir using omegaconf library to override the default value
+    # Initialize digits
+    bg = cv2.imread("conf/bg_digit_240_320.jpg")
+    digits = tacto.Sensor(**cfg.tacto, background=bg)
+
+    # Initialize World
+    global model, data,camera,scene
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    model = mj.MjModel.from_xml_path(f"{current_dir}/../playground_data/touch_playground.xml")
+    data = mj.MjData(model)
+    model.opt.timestep = 0.001
+
+    log.info("Initializing world")
+    # Create GLFW window
+    window = glfw.create_window(1200, 800, "MuJoCo Interactive Viewer", None, None)
+    if not window:
+        glfw.terminate()
+        raise RuntimeError("GLFW window creation failed")
+
+    glfw.make_context_current(window)
+    glfw.set_key_callback(window, key_callback)
+    options = mj.MjvOption()
+    scene = mj.MjvScene(model, maxgeom=1000)
+    context = mj.MjrContext(model, mj.mjtFontScale.mjFONTSCALE_150)
+    camera = mj.MjvCamera()
+
+    # Create and initialize DIGIT
+    # Add object from mujoco to renderer
+    # digits.add_body_mujoco("digit",model,data) # get body in a smart way from mujoco
+    digits.add_body_mujoco("A",model,data) # get body in a smart way from mujoco
+    digits.add_camera_mujoco("touch",model,data) # get camera in a smart way from mujoco
+
+    # Create control panel to control the 6DoF pose of the object
+    # panel = px.gui.PoseControlPanel(obj, **cfg.object_control_panel)
+    # panel.start()
+    log.info("Use the slides to move the object until in contact with the DIGIT")
+
+    # run p.stepSimulation in another thread
+    # t = px.utils.SimulationThread(real_time_factor=1.0)
+    # t.start()
+    # Render the DIGIT sensor in a separate thread
+    def render_digit():
+        while not glfw.window_should_close(window):
+            color, depth = digits.render(model, data)
+            # digits.updateGUI(color, depth)
+            time.sleep(0.01)  # Adjust the sleep time as needed
+    render_thread = threading.Thread(target=render_digit)
+    render_thread.start()
+
+    while not glfw.window_should_close(window):
+        # step simulation of mujoco model
+        mj.mj_step(model, data)
+        # visualize the simulation
+        # Render the scene
+        viewport_width, viewport_height = glfw.get_framebuffer_size(window)
+        mj.mjv_updateScene(
+            model, data, options, None, camera, mj.mjtCatBit.mjCAT_ALL.value, scene
+        )
+        mj.mjr_render(mj.MjrRect(0, 0, viewport_width, viewport_height), scene, context)
+
+        glfw.swap_buffers(window)
+        glfw.poll_events()
+
+
+
+
+
+if __name__ == "__main__":
+    hydra.initialize("conf", version_base=None)
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    cfg=hydra.compose("digit.yaml", overrides=[f"base_dir={script_dir}"])
+    main(cfg)
--- src/sim_in_sparsh/playground/touch_playground_custom_mj_launcher.py
+++ src/sim_in_sparsh/playground/touch_playground_custom_mj_launcher.py
@@ -8,10 +8,13 @@ import time
 
 # Path to your XML model
 current_dir = os.path.dirname(os.path.abspath(__file__))
-MODEL_PATH = f"{current_dir}/playground_data/touch_playground.xml"  # Update with your model path
+MODEL_PATH = (
+    f"{current_dir}/playground_data/touch_playground.xml"  # Update with your model path
+)
 
-CONTROL_INCREMENT = 0.05  # Amount to move joints per keypress
+CONTROL_INCREMENT = 0.01  # Amount to move joints per keypress
 output_data = []  # List to collect sensor data for saving
+
 
 def key_callback(window, key, scancode, action, mods):
     """
@@ -45,26 +48,18 @@ def key_callback(window, key, scancode, action, mods):
         data.qpos[model.joint("rz").qposadr] += CONTROL_INCREMENT
     elif key == glfw.KEY_E:
         data.qpos[model.joint("rz").qposadr] -= CONTROL_INCREMENT
-    # add keys for zooming in and out
-        # Zoom in and out
-    elif key == glfw.KEY_EQUAL:  # Zoom In (e.g., `=` key)
-        data.cam.distance -= CONTROL_INCREMENT
-    elif key == glfw.KEY_MINUS:  # Zoom Out (e.g., `-` key)
-        data.cam.distance += CONTROL_INCREMENT
 
 
 def main():
     # Initialize GLFW
     if not glfw.init():
         raise RuntimeError("GLFW initialization failed")
-    # Print the location of the MuJoCo Python bindings
-    print("MuJoCo Python bindings location:", mj.__file__)
 
     # Load MuJoCo model and data
-    global model, data
+    global model, data,camera,scene
     model = mj.MjModel.from_xml_path(MODEL_PATH)
     data = mj.MjData(model)
-
+    model.opt.timestep = 0.001
 
     # Create GLFW window
     window = glfw.create_window(1200, 800, "MuJoCo Interactive Viewer", None, None)
@@ -95,38 +90,31 @@ def main():
 
         # Fetch touch grid data
         sensor_id = model.sensor("touch").id
-        touch_data = data.sensordata[sensor_id:sensor_id + model.sensor_dim[sensor_id]].reshape((120, 160, 3))  # Adjust shape as per your config
-        # print("Touch Data (Center Pixel):", touch_data[3, 3])  # Example: Print the center pixel
-        print("Touch Data (All Pixels):", touch_data)  # Example: Print all pixels
-        print(touch_data.shape)
-        output_data.append(touch_data)  # Collect data for saving
+        touch_data = data.sensordata[
+            sensor_id : sensor_id + model.sensor_dim[sensor_id]
+        ].reshape(
+            (120, 160, 3)
+        )
 
-        #get the position of the object
-        object_id = mj.mj_name2id(model, mj.mjtObj.mjOBJ_GEOM, "a")
+        # get the position of the object
+        object_id = mj.mj_name2id(model, mj.mjtObj.mjOBJ_GEOM, "digit")
         object_pos = data.geom_xpos[object_id]
 
         # Get position (3D) and rotation matrix (3x3)
-        position = data.geom_xpos[object_id]        # Shape: (3,)
-        rotation = data.geom_xmat[object_id].reshape(3, 3)  # Reshape flattened 9-element array to 3x3
-
-        # Construct 4x4 pose matrix
-        pose_matrix = np.eye(4)
-        pose_matrix[:3, :3] = rotation  # Rotation component
-        pose_matrix[:3, 3] = position   # Translation component
-
-        print("4x4 Pose Matrix:")
-        print(pose_matrix)
+        position = data.geom_xpos[object_id]  # Shape: (3,)
+        rotation = data.geom_xmat[object_id].reshape(
+            3, 3
+        )  # Reshape flattened 9-element array to 3x3
 
         # Render the scene
         viewport_width, viewport_height = glfw.get_framebuffer_size(window)
-        mj.mjv_updateScene(model, data, options, None, camera, mj.mjtCatBit.mjCAT_ALL.value, scene)
+        mj.mjv_updateScene(
+            model, data, options, None, camera, mj.mjtCatBit.mjCAT_ALL.value, scene
+        )
         mj.mjr_render(mj.MjrRect(0, 0, viewport_width, viewport_height), scene, context)
 
         glfw.swap_buffers(window)
         glfw.poll_events()
-
-    # Save collected sensor data to a file
-    print("Touch grid data saved to touch_grid_data.npy")
 
     glfw.terminate()
 
